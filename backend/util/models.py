c96926ff02ae41c13563338b879069986680a482
a1845552SiYuRan <a1845552@adelaide.edu.au>
2024-10-04 02:58:29 +0930

    Backend-01-Complete the Input Template

--- backend/util/models.py
+++ backend/util/models.py
@@ -8,3 +8,18 @@ def login_details(api):
         'username': fields.String(required=True, example='SampleUsername'),
         'password': fields.String(required=True, example='SamplePassword')
     })
+    
+def llm_request_details(api):
+    return api.model('llm_request_details', {
+        'Service': fields.List(fields.String, required=True, example=['Text_Generation', 'Code_Generation']),
+        'Price': fields.Integer(required=True, example='2'),
+        'Response_Speed': fields.Integer(required=True, example='2'),
+        'Accuracy': fields.Integer(required=True, example='2'),
+        'Ethical_Training': fields.Integer(required=True, example='2'),
+        'Green_Computing_Resources': fields.Integer(required=True, example='2'),
+        'Local_Deployment_Capability': fields.Integer(required=True, example='2'),
+        'Training_Resource_Requirements': fields.Integer(required=True, example='2'),
+        'Fine_Tuning_Difficulty': fields.Integer(required=True, example='2'),
+        'Multilingual_Support_Capability': fields.Integer(required=True, example='2'),
+        'Model_Scalability': fields.Integer(required=True, example='2')
+    })
