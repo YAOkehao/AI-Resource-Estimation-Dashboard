f81748894c1ea35425dec4f8aea66fb72b3368ff
a1845552SiYuRan <a1845552@adelaide.edu.au>
2024-10-04 01:09:06 +0930

    Database-01-Building the Database

---
+++ backend/db/init_db.py
@@ -0,0 +1,68 @@
+import csv
+from sqlalchemy import create_engine, Column, Integer, VARCHAR, TEXT
+from sqlalchemy.orm import declarative_base, Session
+
+# 创建引擎
+engine = create_engine('sqlite:///db/dataBase.db?check_same_thread=False', echo=True)
+
+# 声明基类
+Base = declarative_base()
+
+# ORM 模型类
+class LLM(Base):
+    __tablename__ = 'LLM'
+    id = Column(Integer, primary_key=True)
+    Name = Column(VARCHAR(30))
+    Development_Company = Column(VARCHAR(30))
+    Price = Column(Integer)
+    Response_Speed = Column(Integer)
+    Accuracy = Column(Integer)
+    Ethical_Training = Column(Integer)
+    Green_Computing_Resources = Column(Integer)
+    Local_Deployment_Capability = Column(Integer)
+    Training_Resource_Requirements = Column(Integer)
+    Fine_Tuning_Difficulty = Column(Integer)
+    Multilingual_Support_Capability = Column(Integer)
+    Model_Scalability = Column(Integer)
+    Text_Generation = Column(Integer)
+    Image_Generation = Column(Integer)
+    Song_Generation = Column(Integer)
+    Code_Generation = Column(Integer)
+    Table_Processing = Column(Integer)
+    Summarization = Column(Integer)
+    Logical_Reasoning = Column(Integer)
+    Mathematical_Problem_Solving = Column(Integer)
+    Description = Column(TEXT)
+
+    def __repr__(self):
+        return f'LLM:\nid: {self.id}\n'
+
+# 初始化数据库
+def init_db():
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+
+    # 创建 Session 并使用上下文管理器
+    with Session(engine) as session:
+        init_data(session)
+
+# 获取 session
+def get_session():
+    return Session(engine)
+
+# 初始化数据
+def init_data(session):
+    with open('db/data.csv', newline='', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        next(reader)
+        for line in reader:
+            model = LLM(Name=line[0], Development_Company=line[1], Price=int(line[2]), Response_Speed=int(line[3]), 
+                        Accuracy=int(line[4]), Ethical_Training=int(line[5]), Green_Computing_Resources=int(line[6]), 
+                        Local_Deployment_Capability=int(line[7]), Training_Resource_Requirements=int(line[8]), 
+                        Fine_Tuning_Difficulty=int(line[9]), Multilingual_Support_Capability=int(line[10]), 
+                        Model_Scalability=int(line[11]), Text_Generation=int(line[12]), Image_Generation=int(line[13]), 
+                        Song_Generation=int(line[14]), Code_Generation=int(line[15]), Table_Processing=int(line[16]), 
+                        Summarization=int(line[17]), Logical_Reasoning=int(line[18]), 
+                        Mathematical_Problem_Solving=int(line[19]), Description=line[20])
+            session.add(model)
+    session.commit()
